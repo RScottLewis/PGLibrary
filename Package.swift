093c32c37f5991999ae2804bd879c7d299192aea
Scott Lewis <r.scott.lewis@gmail.com>
2023-06-18 21:49:29 -0400

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "PGLibrary",
+    name: "MyFirstSPMDemo",
     platforms: [
       // Only add support for iOS 13 and up.
       .iOS(.v16)
@@ -12,13 +12,13 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PGLibrary",
-            targets: ["PGLibrary"]),
+            name: "MyFirstSPMDemo",
+            targets: ["MyFirstSPMDemo"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        // .package(url: "https://github.com/RScottLewis/PGLibrary", from: "1.0.1"),
+        .package(url: "https://github.com/RScottLewis/PGLibrary", from: "1.0.1"),
         //        .package(url: "https://github.com/RScottLewis/Swifty-Extensions", from: "1.0.1"),
 
 
@@ -27,10 +27,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PGLibrary",
-            dependencies: []),
-        .testTarget(
-            name: "PGLibraryTests",
+            name: "MyFirstSPMDemo",
             dependencies: ["PGLibrary"]),
+        .testTarget(
+            name: "MyFirstSPMDemo",
+            dependencies: ["MyFirstSPMDemo"]),
     ]
 )
